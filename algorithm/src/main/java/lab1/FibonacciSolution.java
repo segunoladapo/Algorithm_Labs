d5f927df74e1f9b4175952a769f031cda9d6057a
segun <segxy4show2000@gmail.com>
2018-07-26 20:21:59 -0500

    new Classes added

--- algorithm/src/main/java/lab1/FibonacciSolution.java
+++ algorithm/src/main/java/lab1/FibonacciSolution.java
@@ -2,22 +2,24 @@ package lab1;
 
 public class FibonacciSolution {
 
-    public static int recursiveFibonacci(int n){
-        if(n == 1 || n == 0)
+    public static int recursiveFibonacci(int n) {
+        if (n == 1 || n == 0)
             return 1;
-        return recursiveFibonacci(n-1) + recursiveFibonacci(n-2);
+        return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
     }
 
-    public static void main(String... args){
-        System.out.println(recursiveFibonacci(5));
+    public static void main(String... args) {
+        System.out.println(iterativeFibonacci(6));
     }
 
-    public static int iteratoveFibonacci(int n){
+    public static int iterativeFibonacci(int n) {
         int total = 0;
         int prev = 1;
-        for(int i = 0; i < n;i++){
-            int temp = prev;
-            total+=prev;
+        for (int i = 0; i < n; i++) {
+            int temp = total;
+            total += prev;
+            prev = temp;
         }
+        return total;
     }
 }
